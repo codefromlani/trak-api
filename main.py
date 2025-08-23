6ab5a578240f1fe85662391afec15330041f0653
codefromlani <rodiathammed48@gmail.com>
2025-08-23 15:25:11 +0100

    feat: add FastAPI app setup with CORS, health check, and routes

---
+++ main.py
@@ -0,0 +1,36 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+from api.db.database import Base, engine
+from api.v1.routes.auth import router as auth_router
+from api.v1.routes.dashboard import router as dashboard_router
+
+load_dotenv()
+
+# Base.metadata.drop_all(bind=engine)
+# print("All tables dropped")
+
+# Recreate tables from models
+Base.metadata.create_all(bind=engine)
+print("All tables created")
+
+app = FastAPI(title="Trak API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:3000")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["health"])
+def health_check():
+    return {"message": f"Server is running and healthy"}
+
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(dashboard_router, prefix="/dashboard", tags=["dashboard"])
